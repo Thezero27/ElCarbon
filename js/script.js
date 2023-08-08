6bdfafbf028edaf2e0061e47d7af2eaa52c50783
Thezero <67761713+Thezero27@users.noreply.github.com>
2023-08-08 15:28:09 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -42,7 +42,7 @@ function validate() {
     if (variable in invitados) {
         invitados[variable]();
         alert("Si estás en lista");
-        window.open("/Restaurant-Fabian/principal.html")
+        window.open("/ElCarbon/principal.html")
     } else {
         alert("NO ESTAS EN LA LISTA \n \n*escribe sin dejar espacios");
     }
